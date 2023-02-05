831fefbc574a2105bd19b9a2eb800fb3dc396e0b
Derek Gooding <djgooding@live.com>
2023-02-05 14:10:55 -0500

    Framework for future updates
    Just added some basic functionality.

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace Radio_Leech
 {
@@ -7,5 +9,49 @@ namespace Radio_Leech
         public MainWindow() => InitializeComponent();
 
         private void Exit_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            string message = $"{Application.Current.MainWindow.Title}\n" +
+                             $"Created by: Derek Gooding\n" +
+                             $"©2023\n" +
+                             $"Version 0.9.0 beta";
+            MessageBox.Show(message);
+        }
+
+        private void Github_Click(object sender, RoutedEventArgs e) => OpenBrowser("https://github.com/DerekGooding/RPGGamers-Radio-Premium");
+
+        private void RadioSite_Click(object sender, RoutedEventArgs e) => OpenBrowser("http://www.rpgamers.net/radio/");
+
+        public static void OpenBrowser(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        
     }
 }
