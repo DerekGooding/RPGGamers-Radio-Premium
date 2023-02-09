98f94868768b1fd79076f9f1026eba7642473713
Derek Gooding <djgooding@live.com>
2023-02-09 10:28:31 -0500

    Import DB
    Automatically pull the existing database file if there is an issue during updating

--- ViewModel/Helpers/DatabaseHelper.cs
+++ ViewModel/Helpers/DatabaseHelper.cs
@@ -2,58 +2,58 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 
 namespace Radio_Leech.ViewModel.Helpers
 {
     public class DatabaseHelper
     {
         private static readonly string dbFile = Path.Combine(Environment.CurrentDirectory, "mvvmDb.db");
+        private static readonly string importUrl = "https://github.com/DerekGooding/RPGGamers-Radio-Premium/raw/main/mvvmDb.db";
 
         public static bool Insert<T>(T item)
         {
-            bool result = false;
-
             using SQLiteConnection connection = new(dbFile);
             connection.CreateTable<T>();
             int rows = connection.Insert(item);
-            if (rows > 0)
-                result = true;
-
-            return result;
+            return rows > 0;
         }
         public static bool Update<T>(T item)
         {
-            bool result = false;
-
             using SQLiteConnection connection = new(dbFile);
             connection.CreateTable<T>();
             int rows = connection.Update(item);
-            if (rows > 0)
-                result = true;
-
-            return result;
+            return rows > 0;
         }
         public static bool Delete<T>(T item)
         {
-            bool result = false;
-
             using SQLiteConnection connection = new(dbFile);
             connection.CreateTable<T>();
             int rows = connection.Delete(item);
-            if (rows > 0)
-                result = true;
-
-            return result;
+            return rows > 0;
         }
 
         public static List<T> Read<T>() where T : new()
         {
-            List<T> items;
+            using SQLiteConnection connection = new(dbFile);
+            connection.CreateTable<T>();
+            List<T> items = connection.Table<T>().ToList();
+            if (items.Count == 0)
+                items = ImportFromOnline<T>();
+            return items;
+        }
+
+        private static List<T> ImportFromOnline<T>() where T : new()
+        {
+            if (System.IO.File.Exists(dbFile)) System.IO.File.Delete(dbFile);
+            using var client = new HttpClient();
+            using var response = client.GetStreamAsync(importUrl);
+            using var stream = new FileStream(dbFile, FileMode.CreateNew);
+            response.Result.CopyToAsync(stream);
 
             using SQLiteConnection connection = new(dbFile);
             connection.CreateTable<T>();
-            items = connection.Table<T>().ToList();
-
+            List<T> items = connection.Table<T>().ToList();
             return items;
         }
 
