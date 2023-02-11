f401f81b804c2632f29ff483a2689bcf0b98172d
Derek Gooding <djgooding@live.com>
2023-02-11 14:02:59 -0500

    quick ups for the manifest

--- obj/Release/net6.0-windows/View/MainWindow.g.i.cs
+++ obj/Release/net6.0-windows/View/MainWindow.g.i.cs
@@ -63,7 +63,7 @@ namespace Radio_Leech {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Radio Leech;V1.0.0.0;component/view/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Radio Leech;component/view/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
