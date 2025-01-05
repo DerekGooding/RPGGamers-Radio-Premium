8f3656afdfdc8de2389b72454cc2d89ec5e8029e
Derek Gooding <djgooding@live.com>
2025-01-05 10:58:34 -0500

    New official release update

--- obj/Release/net7.0-windows/App.g.i.cs
+++ obj/Release/net7.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Radio_Leech {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace Radio_Leech {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Radio Leech;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Radio Leech;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace Radio_Leech {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             Radio_Leech.App app = new Radio_Leech.App();
             app.InitializeComponent();
--- obj/Release/net7.0-windows/Radio Leech.AssemblyInfo.cs
+++ obj/Release/net7.0-windows/Radio Leech.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Radio Leech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab3db05def315d8e80350ebd59ed377afb429f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Radio Leech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Radio Leech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
