a5fd5d6aa4c3aa8bdfa8340b601678d6882d557e
Derek Gooding <djgooding@live.com>
2025-01-10 14:20:18 -0500

    Update SettingsViewModel.cs
    Sandboxable theme change

--- ViewModel/Pages/SettingsViewModel.cs
+++ ViewModel/Pages/SettingsViewModel.cs
@@ -50,7 +50,7 @@ namespace GamerRadio.ViewModel.Pages
                     if (CurrentTheme == ApplicationTheme.Light)
                         break;
 
-                    ApplicationThemeManager.Apply(ApplicationTheme.Light);
+                    Apply(ApplicationTheme.Light);
                     CurrentTheme = ApplicationTheme.Light;
 
                     break;
@@ -59,11 +59,226 @@ namespace GamerRadio.ViewModel.Pages
                     if (CurrentTheme == ApplicationTheme.Dark)
                         break;
 
-                    ApplicationThemeManager.Apply(ApplicationTheme.Dark);
+                    Apply(ApplicationTheme.Dark);
                     CurrentTheme = ApplicationTheme.Dark;
 
                     break;
             }
+        }
+
+                public void Apply(
+        ApplicationTheme applicationTheme,
+        WindowBackdropType backgroundEffect = WindowBackdropType.Mica,
+        bool updateAccent = true)
+        {
+            if (updateAccent)
+            {
+                ApplyAccent(
+                    Color.FromRgb(0, 0, 200),
+                    applicationTheme,
+                    false
+                );
+            }
+
+            if (applicationTheme == ApplicationTheme.Unknown)
+            {
+                return;
+            }
+
+            var themeDictionaryName = applicationTheme == ApplicationTheme.Dark ? "Dark" : "Light";
+
+
+            bool isUpdated = UpdateDictionary("theme",
+                new Uri(ThemesDictionaryPath + themeDictionaryName + ".xaml", UriKind.Absolute)
+            );
+
+            if (UiApplication.Current.MainWindow is Window mainWindow)
+            {
+                WindowBackgroundManager.UpdateBackground(mainWindow, applicationTheme, backgroundEffect);
+            }
+        }
+
+
+        public void ApplyAccent(
+        Color systemAccent,
+        ApplicationTheme applicationTheme = ApplicationTheme.Light,
+        bool systemGlassColor = false
+        )
+        {
+            if (systemGlassColor)
+            {
+                // WindowGlassColor is little darker than accent color
+                systemAccent = systemAccent.UpdateBrightness(6f);
+            }
+
+            Color primaryAccent;
+            Color secondaryAccent;
+            Color tertiaryAccent;
+
+            if (applicationTheme == ApplicationTheme.Dark)
+            {
+                primaryAccent = systemAccent.Update(15f, -12f);
+                secondaryAccent = systemAccent.Update(30f, -24f);
+                tertiaryAccent = systemAccent.Update(45f, -36f);
+            }
+            else
+            {
+                primaryAccent = systemAccent.UpdateBrightness(-5f);
+                secondaryAccent = systemAccent.UpdateBrightness(-10f);
+                tertiaryAccent = systemAccent.UpdateBrightness(-15f);
+            }
+
+            UpdateColorResources(systemAccent, primaryAccent, secondaryAccent, tertiaryAccent);
+        }
+
+        private const double BackgroundBrightnessThresholdValue = 80d;
+
+        private void UpdateColorResources(
+                Color systemAccent,
+                Color primaryAccent,
+                Color secondaryAccent,
+                Color tertiaryAccent
+)
+        {
+            if (secondaryAccent.GetBrightness() > BackgroundBrightnessThresholdValue)
+            {
+                UiApplication.Current.Resources["TextOnAccentFillColorPrimary"] = Color.FromArgb(
+                    0xFF,
+                    0x00,
+                    0x00,
+                    0x00
+                );
+                UiApplication.Current.Resources["TextOnAccentFillColorSecondary"] = Color.FromArgb(
+                    0x80,
+                    0x00,
+                    0x00,
+                    0x00
+                );
+                UiApplication.Current.Resources["TextOnAccentFillColorDisabled"] = Color.FromArgb(
+                    0x77,
+                    0x00,
+                    0x00,
+                    0x00
+                );
+                UiApplication.Current.Resources["TextOnAccentFillColorSelectedText"] = Color.FromArgb(
+                    0x00,
+                    0x00,
+                    0x00,
+                    0x00
+                );
+                UiApplication.Current.Resources["AccentTextFillColorDisabled"] = Color.FromArgb(
+                    0x5D,
+                    0x00,
+                    0x00,
+                    0x00
+                );
+            }
+            else
+            {
+                UiApplication.Current.Resources["TextOnAccentFillColorPrimary"] = Color.FromArgb(
+                    0xFF,
+                    0xFF,
+                    0xFF,
+                    0xFF
+                );
+                UiApplication.Current.Resources["TextOnAccentFillColorSecondary"] = Color.FromArgb(
+                    0x80,
+                    0xFF,
+                    0xFF,
+                    0xFF
+                );
+                UiApplication.Current.Resources["TextOnAccentFillColorDisabled"] = Color.FromArgb(
+                    0x87,
+                    0xFF,
+                    0xFF,
+                    0xFF
+                );
+                UiApplication.Current.Resources["TextOnAccentFillColorSelectedText"] = Color.FromArgb(
+                    0xFF,
+                    0xFF,
+                    0xFF,
+                    0xFF
+                );
+                UiApplication.Current.Resources["AccentTextFillColorDisabled"] = Color.FromArgb(
+                    0x5D,
+                    0xFF,
+                    0xFF,
+                    0xFF
+                );
+            }
+            
+            UiApplication.Current.Resources["SystemAccentColor"] = systemAccent;
+            UiApplication.Current.Resources["SystemAccentColorPrimary"] = primaryAccent;
+            UiApplication.Current.Resources["SystemAccentColorSecondary"] = secondaryAccent;
+            UiApplication.Current.Resources["SystemAccentColorTertiary"] = tertiaryAccent;
+
+            UiApplication.Current.Resources["SystemAccentBrush"]                          = new SolidColorBrush(secondaryAccent);
+            UiApplication.Current.Resources["SystemFillColorAttentionBrush"]              = new SolidColorBrush(secondaryAccent);
+            UiApplication.Current.Resources["AccentTextFillColorPrimaryBrush"]            = new SolidColorBrush(tertiaryAccent);
+            UiApplication.Current.Resources["AccentTextFillColorSecondaryBrush"]          = new SolidColorBrush(tertiaryAccent);
+            UiApplication.Current.Resources["AccentTextFillColorTertiaryBrush"]           = new SolidColorBrush(secondaryAccent);
+            UiApplication.Current.Resources["AccentFillColorSelectedTextBackgroundBrush"] = new SolidColorBrush(systemAccent);
+            UiApplication.Current.Resources["AccentFillColorDefaultBrush"]                = new SolidColorBrush(secondaryAccent);
+
+            UiApplication.Current.Resources["AccentFillColorSecondaryBrush"]              = new SolidColorBrush(secondaryAccent) { Opacity = 0.9};
+            UiApplication.Current.Resources["AccentFillColorTertiaryBrush"]               = new SolidColorBrush(secondaryAccent) { Opacity = 0.8 };
+        }
+
+        public bool UpdateDictionary(string resourceLookup, Uri? newResourceUri)
+        {
+            Collection<ResourceDictionary> applicationDictionaries = UiApplication
+                .Current
+                .Resources
+                .MergedDictionaries;
+
+            if (applicationDictionaries.Count == 0 || newResourceUri is null)
+            {
+                return false;
+            }
+
+            resourceLookup = resourceLookup.ToLower().Trim();
+
+            for (int i = 0; i < applicationDictionaries.Count; i++)
+            {
+                string sourceUri;
+
+                if (applicationDictionaries[i]?.Source != null)
+                {
+                    sourceUri = applicationDictionaries[i].Source.ToString().ToLower().Trim();
+
+                    if (sourceUri.Contains(LibraryNamespace) && sourceUri.Contains(resourceLookup))
+                    {
+                        applicationDictionaries[i] = new() { Source = newResourceUri };
+
+                        return true;
+                    }
+                }
+
+                for (int j = 0; j < applicationDictionaries[i].MergedDictionaries.Count; j++)
+                {
+                    if (applicationDictionaries[i].MergedDictionaries[j]?.Source == null)
+                    {
+                        continue;
+                    }
+
+                    sourceUri = applicationDictionaries[i]
+                        .MergedDictionaries[j]
+                        .Source.ToString()
+                        .ToLower()
+                        .Trim();
+
+                    if (!sourceUri.Contains(LibraryNamespace) || !sourceUri.Contains(resourceLookup))
+                    {
+                        continue;
+                    }
+
+                    applicationDictionaries[i].MergedDictionaries[j] = new() { Source = newResourceUri };
+
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
